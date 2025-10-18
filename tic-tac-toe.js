565676eb50e8e7142108cf05e4064542c1061d4b
Shevaunie Senior <shevaunie.senior@mymona.uwi.edu>
2025-10-17 23:57:54 -0500

    tested checker done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -24,32 +24,103 @@ document.addEventListener('DOMContentLoaded',function(){
 			}
 	}
 
-/*
+
 	let sco_chk = function(mk){
 			let elem = document.getElementById("status");
-				if((boxes[0].textContent===boxes[1].textContent) && (boxes[0].textContent===boxes[2].textContent)){
-					console.log("check done");
-					elem.textContent = "Congratulations!"+ mk+" is the Winner";
+			if (boxes[0].textContent!=="" && boxes[1].textContent!== "" && boxes[2].textContent!==""){
+				if(boxes[0].textContent==="X" && boxes[1].textContent=== "X" && boxes[2].textContent==="X"){
+					elem.textContent = "Congratulations! X is the Winner";
 					elem.classList.add("you-won");	
 				}
+				if(boxes[0].textContent==="O" && boxes[1].textContent=== "O" && boxes[2].textContent==="O"){
+					elem.textContent = "Congratulations! O is the Winner";
+					elem.classList.add("you-won");	
+				}
+			}
+			if (boxes[3].textContent!=="" && boxes[4].textContent!== "" && boxes[5].textContent!==""){
+				if(boxes[3].textContent==="X" && boxes[4].textContent=== "X" && boxes[5].textContent==="X"){
+					elem.textContent = "Congratulations! X is the Winner";
+					elem.classList.add("you-won");	
+				}
+				if(boxes[3].textContent==="O" && boxes[4].textContent=== "O" && boxes[5].textContent==="O"){
+					elem.textContent = "Congratulations! O is the Winner";
+					elem.classList.add("you-won");	
+				}
+			}
 
-			
-				if((boxes[3].textContent===boxes[4].textContent) && (boxes[3].textContent===boxes[5].textContent)){
-					console.log("check done");
-					elem.textContent = "Congratulations!"+ mk+" is the Winner";
+			if (boxes[6].textContent!=="" && boxes[7].textContent!== "" && boxes[8].textContent!==""){
+				if(boxes[6].textContent==="X" && boxes[7].textContent=== "X" && boxes[8].textContent==="X"){
+					elem.textContent = "Congratulations! X is the Winner";
 					elem.classList.add("you-won");	
 				}
+				if(boxes[6].textContent==="O" && boxes[7].textContent=== "O" && boxes[8].textContent==="O"){
+					elem.textContent = "Congratulations! O is the Winner";
+					elem.classList.add("you-won");	
+				}
+			}
 
-			
-				if((boxes[6].textContent===boxes[7].textContent) && (boxes[6].textContent===boxes[8].textContent)){
-					console.log("check done");
-					elem.textContent = "Congratulations!"+ mk+" is the Winner";
+			///////////////////////////////////////////////////
+						if (boxes[0].textContent!=="" && boxes[6].textContent!== "" && boxes[3].textContent!==""){
+				if(boxes[0].textContent==="X" && boxes[6].textContent=== "X" && boxes[3].textContent==="X"){
+					elem.textContent = "Congratulations! X is the Winner";
 					elem.classList.add("you-won");	
 				}
-						
+				if(boxes[0].textContent==="O" && boxes[3].textContent=== "O" && boxes[6].textContent==="O"){
+					elem.textContent = "Congratulations! O is the Winner";
+					elem.classList.add("you-won");	
+				}
+			}
+			if (boxes[1].textContent!=="" && boxes[4].textContent!== "" && boxes[7].textContent!==""){
+				if(boxes[1].textContent==="X" && boxes[4].textContent=== "X" && boxes[7].textContent==="X"){
+					elem.textContent = "Congratulations! X is the Winner";
+					elem.classList.add("you-won");	
+				}
+				if(boxes[1].textContent==="O" && boxes[4].textContent=== "O" && boxes[7].textContent==="O"){
+					elem.textContent = "Congratulations! O is the Winner";
+					elem.classList.add("you-won");	
+				}
+			}
+
+			if (boxes[2].textContent!=="" && boxes[5].textContent!== "" && boxes[8].textContent!==""){
+				if(boxes[2].textContent==="X" && boxes[5].textContent=== "X" && boxes[8].textContent==="X"){
+					elem.textContent = "Congratulations! X is the Winner";
+					elem.classList.add("you-won");	
+				}
+				if(boxes[2].textContent==="O" && boxes[5].textContent=== "O" && boxes[8].textContent==="O"){
+					elem.textContent = "Congratulations! O is the Winner";
+					elem.classList.add("you-won");	
+				}
+			}
+
+			///////////////////////////////////////////////////////////////
+
+			if (boxes[0].textContent!=="" && boxes[4].textContent!== "" && boxes[8].textContent!==""){
+				if(boxes[0].textContent==="X" && boxes[4].textContent=== "X" && boxes[8].textContent==="X"){
+					elem.textContent = "Congratulations! X is the Winner";
+					elem.classList.add("you-won");	
+				}
+				if(boxes[0].textContent==="O" && boxes[4].textContent=== "O" && boxes[8].textContent==="O"){
+					elem.textContent = "Congratulations! O is the Winner";
+					elem.classList.add("you-won");	
+				}
+			}
+
+			if (boxes[2].textContent!=="" && boxes[4].textContent!== "" && boxes[6].textContent!==""){
+				if(boxes[0].textContent==="X" && boxes[4].textContent=== "X" && boxes[6].textContent==="X"){
+					elem.textContent = "Congratulations! X is the Winner";
+					elem.classList.add("you-won");	
+				}
+				if(boxes[2].textContent==="O" && boxes[4].textContent=== "O" && boxes[6].textContent==="O"){
+					elem.textContent = "Congratulations! O is the Winner";
+					elem.classList.add("you-won");	
+				}
+			}
+					/*console.log("check done");
+					elem.textContent = "Congratulations!"+ mk+" is the Winner";
+					elem.classList.add("you-won");*/							
 		}
 
-*/
+
 	let play = function(){
 		restart.addEventListener('click',clearBoard);
 		let currMk ='X';
@@ -61,6 +132,7 @@ document.addEventListener('DOMContentLoaded',function(){
 			if(clickedBx.textContent===""){
 				clickedBx.textContent = currMk;
 				clickedBx.classList.add(currMk);
+				sco_chk(currMk);
 				if(currMk==="X"){
 					currMk = "O";	
 					} else {

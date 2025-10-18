fbccddf358e8741d70549b663a6e205714db0de7
Shevaunie Senior <shevaunie.senior@mymona.uwi.edu>
2025-10-17 22:09:34 -0500

    Restart code complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,16 +5,26 @@
 document.addEventListener('DOMContentLoaded',function(){
 	let  brd = document.getElementById("board");
 	let boxes = brd.getElementsByTagName("div");
-
+	let restart = document.querySelector(".btn");
 
 	let drawBoard = function(){
-		//1. Layout Board
+			//1. Layout Board
 		for(let x =0; x < boxes.length; x++){
 			boxes[x].className = "square";};
 		}
+	
+
+	let clearBoard = function(){
+		for(box of boxes){
+			drawBoard();
+			box.textContent = "";
+			}
+	}
 
 
-	let play = function(ch){
+
+
+	let play = function(){
 		let currMk ='X';
 		for(box of boxes){
 			box.addEventListener('click', function(event){ 
@@ -29,24 +39,18 @@ document.addEventListener('DOMContentLoaded',function(){
 					}
 				}
 			});
-		drawBoard();
+			drawBoard();
 		}
 	}
+	restart.addEventListener('click',clearBoard);
 
 	let init = function(){
 		alert("game started\n");
 		drawBoard();
 		play();
-	}
+		}
 
 	init();
-	
-
-//2. Add X or O
-	//boxes[x].addEventListener("click", function(){})
-	//3. Mouse/Square style change
-//4. Check winner & Update State
-//5. Restart
-//6. Disallow cheating
+		
 });
 
